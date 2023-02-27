4fb7365afe085edf10c27697c3c54907f2628d86
Rishimithun Muralidharan <rishimithun123@gmail.com>
2023-02-27 13:53:50 +0530

    working prototype

--- madlibs.php
+++ madlibs.php
@@ -7,11 +7,33 @@
   <body>
     <h1>Mad Libs Game</h1>
     <form method="get" acton="madlibs.php">
+      Color:<input type="text" name="color" >
+      <br>
+      Plural noun: <input type="text" name="pluralnoun">
+      <br>
+      Celebrity: <input type="text" name="celebrity">
+      <br>
       <input type="submit">
     </form>
-    <br/>
+    <br>
     <?php
-    
+    error_reporting(0);
+    $color = $_GET["color"]
+    ?>
+    <br>
+    <?php
+    $pluralnoun = $_GET["pluralnoun"]
+    ?>
+    <br>
+    <?php
+    $celebrity = $_GET["celebrity"]
+    ?>
+    <br>
+    <?php
+    echo "Roses are $color<br/>";
+    echo "$pluralnoun are blue<br/>";
+    echo "I love $celebrity<br/>";
+
     ?>
 
   </body>
